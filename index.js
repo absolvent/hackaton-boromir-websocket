2b1d557e869997c37e1ca3bd558c632debcf9d3a
Krzysztof Proczek <krzysztof.proczek@absolvent.pl>
2019-03-29 12:17:40 +0100

    .

--- index.js
+++ index.js
@@ -7,11 +7,46 @@ const app = express()
 app.use(helmet())
 
 app.get('*', (req, res) => {
-    res.set('Content-Type', 'text/html')
-    res.send(200, `
-        <h1><marquee direction=right>Hello from Express path '/' on Now 2.0!</marquee></h1>
-        <h2>Go to <a href="/about">/about</a></h2>
+  res.set('Content-Type', 'text/html')
+  res.send(200, `
+        WEBSOCKET
     `)
+})
+
+const WebSocket = require('ws')
+const wss = new WebSocket.Server({ port: 8080 })
+let DB = {}
+
+wss.on('connection', function connection(ws) {
+  ws.on('message', function incoming(message) {
+    switch (message) {
+      case 'CLEAR': {
+        DB = {}
+        wss.clients.forEach(function each(client) {
+          if (client.readyState === WebSocket.OPEN) {
+            client.send('CLEAR')
+          }
+        })
+        break
+      }
+      case 'REVEAL': {
+        const arr = Object.values(DB)
+        const average = arr.reduce((acc, curr) => acc + curr, 0) / arr.length
+
+        wss.clients.forEach(function each(client) {
+          if (client.readyState === WebSocket.OPEN) {
+            client.send(average)
+          }
+        })
+        break
+      }
+      default: {
+        const { id, num } = JSON.parse(message)
+        DB[id] = num
+        break
+      }
+    }
+  })
 })
 
 module.exports = app
