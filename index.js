df7fabce5072543ad46a65c71bf3c5b9b145e312
Krzysztof Proczek <krzysztof.proczek@absolvent.pl>
2019-03-29 12:23:14 +0100

    .

--- index.js
+++ index.js
@@ -13,40 +13,41 @@ app.get('*', (req, res) => {
     `)
 })
 
-// const WebSocket = require('ws')
-// const wss = new WebSocket.Server({ port: 8080 })
-// let DB = {}
-//
-// wss.on('connection', function connection(ws) {
-//   ws.on('message', function incoming(message) {
-//     switch (message) {
-//       case 'CLEAR': {
-//         DB = {}
-//         wss.clients.forEach(function each(client) {
-//           if (client.readyState === WebSocket.OPEN) {
-//             client.send('CLEAR')
-//           }
-//         })
-//         break
-//       }
-//       case 'REVEAL': {
-//         const arr = Object.values(DB)
-//         const average = arr.reduce((acc, curr) => acc + curr, 0) / arr.length
-//
-//         wss.clients.forEach(function each(client) {
-//           if (client.readyState === WebSocket.OPEN) {
-//             client.send(average)
-//           }
-//         })
-//         break
-//       }
-//       default: {
-//         const { id, num } = JSON.parse(message)
-//         DB[id] = num
-//         break
-//       }
-//     }
-//   })
-// })
+const WebSocket = require('ws')
+const wss = new WebSocket.Server({ port: 443 })
+
+let DB = {}
+
+wss.on('connection', function connection(ws) {
+  ws.on('message', function incoming(message) {
+    switch (message) {
+      case 'CLEAR': {
+        DB = {}
+        wss.clients.forEach(function each(client) {
+          if (client.readyState === WebSocket.OPEN) {
+            client.send('CLEAR')
+          }
+        })
+        break
+      }
+      case 'REVEAL': {
+        const arr = Object.values(DB)
+        const average = arr.reduce((acc, curr) => acc + curr, 0) / arr.length
+
+        wss.clients.forEach(function each(client) {
+          if (client.readyState === WebSocket.OPEN) {
+            client.send(average)
+          }
+        })
+        break
+      }
+      default: {
+        const { id, num } = JSON.parse(message)
+        DB[id] = num
+        break
+      }
+    }
+  })
+})
 
 module.exports = app
